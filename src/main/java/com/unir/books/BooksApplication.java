f893dd52488a64d93b40030362b107b7d28de46d
Sebastian Guerrero <sebastianfelipe.guerrero224@comunidadunir.net>
2024-02-23 11:37:05 -0500

    commit: Se ajustan parametros para despliegue

--- src/main/java/com/unir/books/BooksApplication.java
+++ src/main/java/com/unir/books/BooksApplication.java
@@ -8,6 +8,9 @@ public class BooksApplication {
 
 
 	public static void main(String[] args) {
+
+		String profile = System.getenv("PROFILE");
+		System.setProperty("spring.profiles.active", profile != null ? profile : "default");
 		SpringApplication.run(BooksApplication.class, args);
 	}
 
